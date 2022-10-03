1f6d64e0bf43a68b9f73780a304f013d10cd1bc3
Lal <amrit.b.lal@capgemini.com>
2022-10-03 10:24:38 +0530

    basic questions

---
+++ js-concepts/003basicQuestions.js
@@ -0,0 +1,33 @@
+// Difference between “ == “ and “ === “ operators.
+
+// Both are comparison operators
+// The difference between both the operators is that
+//  “==” is used to compare values whereas
+// “ === “ is used to compare both values and types
+
+var x = 2;
+var y = '2';
+console.log("x==y",x==y,"x===y",x===y);
+
+
+//var vs let vs const
+// --> var is function scope 
+// --> let and const is block level scoped
+// **************************** In ECMAScript 2015, let and const are hoisted but not initialized. Referencing the variable in the block before the variable declaration results in a ReferenceError because the variable is in a "temporal dead zone" from the start of the block until the declaration is processed
+
+
+// Is javascript a statically typed or a dynamically typed language?
+// JavaScript is a dynamically typed language
+
+
+// What is NaN property in JavaScript?
+// NaN property represents the “Not-a-Number” value. It indicates a value that is not a legal number.
+// typeof of NaN will return a Number.
+// To check if a value is NaN, we use the isNaN() function,
+
+
+// Explain passed by value and passed by reference.
+// In JavaScript, primitive data types are passed by value and 
+// non-primitive data types are passed by reference.
+
+
