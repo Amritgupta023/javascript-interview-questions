07812873efe7d1c6f32e4263a28bca135ffcdfbf
Lal <amrit.b.lal@capgemini.com>
2022-10-04 12:45:26 +0530

    promise

--- js-problems/promise.js
+++ js-problems/promise.js
@@ -1,1 +1,17 @@
 //promise.
+
+var p1 = new Promise((resolve,reject)=>{
+    if(true){
+        resolve('this is resolved promise');
+    }else{
+        reject('reject');
+    }
+})
+
+p1.then((data)=>{
+    console.log('data',data);
+}).catch((err)=>{
+    console.log('if error');
+}).finally((data)=>{
+    console.log('finally run');
+})
